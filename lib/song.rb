ad9aecfc9fb765a6a6bdbe5b1afaf669391d3e40
Steven Topazio <sat@northpointprinting.com>
2020-09-30 19:55:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def initialize(song_name, artist, genre)
     @name = song_name 
     @artist = artist 
-    @genres = genre 
+    @genre = genre 
     @@count += 1 
     @@genres << genre 
     @@artists << artist 
