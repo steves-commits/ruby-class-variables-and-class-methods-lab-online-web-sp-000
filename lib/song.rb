146b797ca9c7f2ab9ea1cabb2245449049e99507
Steven Topazio <sat@northpointprinting.com>
2020-09-30 19:43:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,15 @@ class Song
  
  attr_accessor :name, :artist, :genre 
  
- @@count = 0 
+ @@count = 0
+ @@genre = []
+ 
  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
-    @@count +=
+    @@count += 1 
   end
   
   
